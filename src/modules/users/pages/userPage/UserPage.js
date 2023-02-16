8e5941522c2ce09c4a30538307565595c6bbbdb0
Vitalii Matviienko <doktormc25@gmail.com>
2023-02-16 08:33:40 +0200

    minor chage

--- src/modules/users/pages/userPage/UserPage.js
+++ src/modules/users/pages/userPage/UserPage.js
@@ -1,7 +1,7 @@
 import React,{ useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchUsers } from "../../../../store/actions/userActions";
-import UserList from '../../components/UsersList/UsersList'
+import UsersList from "../../components/usersList/UsersList";
 import "./UserPage.css";
 
 const UserPage = () => {
@@ -23,7 +23,7 @@ const UserPage = () => {
         <div>Phone number</div>
         <div>E-mail</div>
       </div>
-      <UserList/>
+     <UsersList/>
     </>
   );
 };
